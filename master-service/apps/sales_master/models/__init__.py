6104f165f6c407da06ccfb8d00578e9f3b419532
ZigmaSoftware <creative@zigma.in>
2026-07-20 17:28:58 +0530

    sales module added

---
+++ master-service/apps/sales_master/models/__init__.py
@@ -0,0 +1,7 @@
+from .scrap_sales_category_master import ScrapSalesCategoryMaster
+from .item_type_master import ItemTypeMaster
+
+__all__ = [
+    "ScrapSalesCategoryMaster",
+    "ItemTypeMaster",
+]
